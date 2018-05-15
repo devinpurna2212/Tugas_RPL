11a3fe35ae1d39f3d5c9235067b02e5e446a3a5a
Husna Nurarifah <Husna Nurarifah>
2018-05-16 05:38:35 +0700

    signupdate

--- src/pages/sign/sign.ts
+++ src/pages/sign/sign.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
-import { NgForm } from '@angular/forms';
+import { NavController, NavParams, AlertController, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
-import { HomePage } from '../home/home';
 import { ProfilPage } from '../profil/profil';
 
 
@@ -12,10 +10,12 @@ import { ProfilPage } from '../profil/profil';
 })
 export class SignPage {
   data:any = {};
+  regexp = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
   constructor(public navCtrl: NavController, 
               public alertCtrl: AlertController, 
               public navParams: NavParams, 
+              public toastCtrl: ToastController,
               public http: Http) {
     this.data.fullname = '';
     this.data.email = '';
@@ -41,6 +41,26 @@ export class SignPage {
     alert.present();
   }
 
+  check(){
+    if(this.data.name === '' || this.data.username === '' || this.data.email === '' || this.data.password === '' || this.data.repassword === ''){
+      let toast = this.toastCtrl.create({
+        message: 'Please fill all the data field',
+        duration: 2000,
+        position: 'bottom'
+      });
+      toast.present();
+    } else if(this.regexp.test(this.data.email) == false) {
+      let toast = this.toastCtrl.create({
+        message: 'Please check your email address',
+        duration: 2000,
+        position: 'bottom'
+      });
+      toast.present();
+    } else {
+      this.signUp();
+    }
+  }
+
   signUp(){
     var link = 'http://localhost/backend/register.php';
     var newUser = JSON.stringify({fullname: this.data.fullname, username: this.data.username, email: this.data.email, password: this.data.password})
@@ -52,5 +72,9 @@ export class SignPage {
   }, error => {
     console.log("Oooops!");
   });
+  }
+
+  ionViewDidLoad(){
+    console.log('ionViewDidLoad SignPage');
   }
 }
