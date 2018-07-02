92d5aedbd44d3c5e715b4e757e695e071899be2b
husnarfh <husna.nna@gmail.com>
2018-07-02 19:40:48 +0700

    Update data.ts

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,50 +1,51 @@
-import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Storage } from '@ionic/storage';
-/*
-  Generated class for the DataProvider provider.
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
+
 @Injectable()
 export class Data {
-  public BASE_URL = 'http://wahsampah2.atspace.cc';
-  public HAS_LOGGED_IN = 'status_login';
 
-  constructor(public http: Http, public storage: Storage) {
-    console.log('Hello Data Provider');
+  public BASE_URL = 'http://localhost/backend';
+  public HAS_LOGGED_IN = 'status_login';
+  
+  constructor(public http: Http , 
+              public storage: Storage) {
+  }
+
+ LoadSampah() {
+    var url = 'http://localhost/backend/load-barang.php';
+    var response = this.http.get(url).map(res => res.json());
+    return response;
   }
   
-
-
-  login(data : any,role:string) {
+ login(data: any, role:string) {
     this.storage.set(this.HAS_LOGGED_IN, true);
     this.storage.set('user_data', data);
     this.storage.set('role', role);
   };
-  
 
-  logout() {
+logout() {
     this.storage.remove(this.HAS_LOGGED_IN);
-    this.storage.remove('user_data');
-    this.storage.remove('role');
+    this.storage.remove('user_user');
   };
 
-  isLogin(){
+isLogin(){
     return this.storage.get(this.HAS_LOGGED_IN).then((value)=>{
-      return value;
+    return value;
     });
   }
 
-  getRole(){
+getRole(){
     return this.storage.get('role').then((value)=>{
-      return value;
+    return value;
     });
   }
-  getData() {
-    return this.storage.get('user_data').then((value) => {
-      return value;
+
+getData() {
+    return this.storage.get('user_user').then((value) => {
+    return value;
     });
-}
+  }
+
 }
